f61d6a3069156f6bf4f8f0d634b4f2c404287bc9
trdmitr <trdmitr@gmail.com>
2023-11-26 20:09:55 +0400

    Updates

---
+++ precache-manifest.256beff10301355c98d57587290891bf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6f0214008d1337656ef",
+    "url": "/BG-2023/static/css/main.2612207f.chunk.css"
+  },
+  {
+    "revision": "d6f0214008d1337656ef",
+    "url": "/BG-2023/static/js/main.68ef1919.chunk.js"
+  },
+  {
+    "revision": "2fa7a5e1664a53210841",
+    "url": "/BG-2023/static/js/runtime~main.4b89be5c.js"
+  },
+  {
+    "revision": "bf7a2d0ec546e9342fe3",
+    "url": "/BG-2023/static/css/2.c86f49e9.chunk.css"
+  },
+  {
+    "revision": "bf7a2d0ec546e9342fe3",
+    "url": "/BG-2023/static/js/2.744c8687.chunk.js"
+  },
+  {
+    "revision": "f357f7882a4bd5bcec5f",
+    "url": "/BG-2023/static/js/3.37352ce7.chunk.js"
+  },
+  {
+    "revision": "f57c1202a8a62acee8a56622412c3e5b",
+    "url": "/BG-2023/static/media/jakobctt_8b806cbf869f1c45daf1949a025cd519.f57c1202.woff"
+  },
+  {
+    "revision": "4843522e1a3181c95bb5d3e073fbf4b2",
+    "url": "/BG-2023/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d9840900b19ef9b2f7c8e0399fda760d.js"
+  "/BG-2023/precache-manifest.256beff10301355c98d57587290891bf.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/BG-2023/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
